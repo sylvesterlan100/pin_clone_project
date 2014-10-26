8e0b1e6b48f4c71db13323943fc1552e558af201
sylvesterlan100 <sylvesterlan100@gmail.com>
2014-10-26 09:34:07 -0700

    third commit

--- Current_Pinterest/app.js
+++ Current_Pinterest/app.js
@@ -10,14 +10,67 @@ app.controller("postCtrl", function($scope, $firebase){
 
 //ADD OBJECT TO DATABASE
 
-	$scope.addPost = function(w, x, y, z){
+	$scope.addPost = function(w, x, y, z, a){
 		
+		
+			if(w===undefined||w===null)
+				wedit="";
+			else
+				wedit=w;
 
-		$scope.page.$add({ image:w, title:x, category:y, userComment:z, comment:"", liked:"false" }); // firebase gives us the ability to use this $add function
-		// $scope.image = "";
-		// $scope.title = "";
-		// $scope.category = "";
-		// $scope.userComment = "";
+			if(x===undefined||x===null)
+				xedit="";
+			else
+				xedit=x;
+
+			if(y===undefined||y===null)
+				yedit="";
+			else
+				yedit=y;
+
+			if(z===undefined||z===null)
+				zedit="";
+			else
+				zedit=z;
+			
+			
+			if(a===undefined)
+				aedit=[""];
+			else if(a.indexOf(",")===-1)
+			{
+				aedit=a.split(" ");
+			}
+			else
+			{
+				aedit=a.split(", ");
+			}
+
+			console.log(wedit)
+			console.log(zedit)
+			if(zedit===""&&wedit==="")
+			{
+				alert("Please leave a comment or picture!");
+				$("#myModal").modal('hide');
+				$scope.image = "";
+				$scope.title = "";
+				$scope.category = "";
+				$scope.userComment = "";
+				$scope.tags=""
+
+
+			}
+			else
+			{
+				$scope.page.$add({ image:wedit, title:xedit, category:yedit, userComment:zedit, comment:"", liked:"false", tags:aedit }); // firebase gives us the ability to use this $add function
+				$scope.image = "";
+				$scope.title = "";
+				$scope.category = "";
+				$scope.userComment = "";
+				$scope.tags=""
+				$("#myModal").modal('hide');
+
+			}
+
 	}
 
 
